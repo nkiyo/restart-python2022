9a6c971c0a8173b18398d37cc111341e02d2ca9a
nkiyo <hoge@hoge.com>
2022-02-08 08:35:28 +0900

    Update

--- hoge.py
+++ hoge.py
@@ -2,21 +2,21 @@ from typing import Tuple
 
 
 class MyClass:
-    i = 12345
+    i: int = 12345
 
     def __init__(self):
         self.i = 23456
 
-    def f(self, x):
+    def f(self, x: str) -> None:
         print("hello, " + x)
 
-    def add(self, x: Tuple[int, int, str]):
+    def add(self, x: Tuple[int, int, str]) -> int:
         n1, n2, s = x
         print(f"{s}")
         return n1 + n2
 
 
-x = MyClass()
+x: MyClass = MyClass()
 x.f("kiyoshi")
 
 print(f"add is {x.add((1,2, 'hoge'))}")
